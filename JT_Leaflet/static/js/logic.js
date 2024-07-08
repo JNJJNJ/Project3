03ff1c824a465e0c681948b116649bc2856ce20c
scorpionhand <162941137+scorpionhand@users.noreply.github.com>
2024-07-08 14:39:50 -0500

    Logic.js cleanup

--- JT_Leaflet/static/js/logic.js
+++ JT_Leaflet/static/js/logic.js
@@ -1,12 +1,20 @@
-// URL for the GeoJSON earthquake data - all earthquakes in the past 7 days
+// URL for the GeoJSON UAP data
 let url = "../UAP_Data/uap_data_output.geojson";
 // https://github.com/JNJJNJ/Project3/blob/main/UAP_Data/uap_data_output.geojson
 
-
+// Globals 
+let dataset = d3.json(url)
+let geoLayer
 let shape_filter = 'All'
 let markerRadius = .1
 let shapeColor = '#FFFF00'
 let markersOnMap = 10000
+let sightingsCount = 0
+let sightingSummary = ''
+let sightingCity = ''
+let sightingState = ''
+let sightingOccurred = ''
+
 
 // Initiate the Leaflet map
 let uap_map = L.map("map", {
@@ -25,6 +33,7 @@ var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/a
 });
 Stadia_AlidadeSmoothDark.addTo(uap_map);
 
+// Shape list
 let shapes = [
     "Changing",
     "Chevron",
@@ -50,66 +59,19 @@ let shapes = [
     "Teardrop",
     "Triangle"
     ]
-//Leaflet circleMarker: fillColor property - marker color corresponding to earthquake depth
+
+
+// Leaflet Marker Color: 
 function shape_color(shape) {
     return shapeColor
-    switch (true) {
-        case shape == 'Changing':
-            return "#F08784";
-        case shape == 'Chevron':
-            return "#EB3324";
-        case shape == 'Cigar':
-            return "#774342";
-        case shape == 'Circle':
-            return "#9FFCFD";
-        case shape == 'Cone':
-            return "#3282F6";
-        case shape == 'Cross':
-            return "#0023F5";
-        case shape == 'Cube':
-            return "#16417C";
-        case shape == 'Cylinder':
-            return "#000C7B";
-        case shape == 'Diamond':
-            return "#FFFE91";
-        case shape == 'Disk':
-            return "#FFFD55";
-        case shape == 'Egg':
-            return "#F09B59";
-        case shape == 'Fireball':
-            return "#F08650";
-        case shape == 'Flash':
-            return "#784315";
-        case shape == 'Formation':
-            return "#817F26";
-        case shape == 'Light':
-            return "#7E84F7";
-        case shape == 'Orb':
-            return "#C0C0C0";
-        case shape == 'Other':
-            return "#7F82BB";
-        case shape == 'Oval':
-            return "#EA3680";
-        case shape == 'Rectangle':
-            return "#EA3FF7";
-        case shape == 'Sphere':
-            return "#75FA8D";
-        case shape == 'Star':
-            return "#75FA61";
-        case shape == 'Teardrop':
-            return "#507F80";
-        case shape == 'Triangle':
-            return "#000000";
-        default:
-            return "#FFFFFF";
-    }
 }
 
+// Using circle marker
 function setMarkers(feature, latlng) {
     return L.circleMarker(latlng, marker_options(feature));
 }
 
-//Leaflet circleMarker: properties
+// Leaflet Marker Properties
 function marker_options(feature) {
     if(markersOnMap < 500){
         markerRadius = 3
@@ -124,11 +86,19 @@ function marker_options(feature) {
     };   
 }
 
-// Leaflet onEachFeature: properties
+// Leaflet onEachFeature: properties 
 function each_feature(feature, layer) {
     let sight_date = new Date(feature.properties.Occurred);
     sight_date = (sight_date.getMonth()+1) + '/' + sight_date.getDate() + '/' + sight_date.getFullYear(); 
     
+    // update globals here
+    sightingsCount += 1 
+    sightingSummary = feature.properties.Summary
+    sightingCity = feature.properties.City
+    sightingState = feature.properties.State
+    sightingOccurred = feature.properties.Occurred
+
+    // add popup
     layer.bindPopup(
         "<h3>Date: " + sight_date + "</h3>" +
         "<h4> UAP Shape: " + feature.properties.Shape + "</h4>" +
@@ -139,60 +109,44 @@ function each_feature(feature, layer) {
 
 }
 
-// Legend for map color depth
-function make_legend(){
-    let legend = L.DomUtil.create("div", "legend"),
-        depth = ['Oval', 'Rectangle', 'Sphere', 'Star', 'Teardrop', 'Triangle'];
-        legend.innerHTML +=
-            '<b>Shapes</b> <br />' +
-            '<span style="background:' + shape_color(depth[0]) + ';">&nbsp&nbsp&nbsp</span> ' + depth[0] + '<br />' +
-            '<span style="background:' + shape_color(depth[1]) + ';">&nbsp&nbsp&nbsp</span> ' + depth[1] + '<br />' +
-            '<span style="background:' + shape_color(depth[2]) + ';">&nbsp&nbsp&nbsp</span> ' + depth[2] + '<br />' +
-            '<span style="background:' + shape_color(depth[3]) + ';">&nbsp&nbsp&nbsp</span> ' + depth[3] + '<br />' +
-            '<span style="background:' + shape_color(depth[4]) + ';">&nbsp&nbsp&nbsp</span> ' + depth[4] + '<br />' +
-            '<span style="background:' + shape_color(depth[5]) + ';">&nbsp&nbsp&nbsp</span> ' + depth[5] + '<br />'
-
-    return legend;
-}
-
-// Leaflet layer for the map legend
-function add_legend(uap_map){
-    let legend = L.control({position: "topright"});
-    legend.onAdd = make_legend
-    legend.addTo(uap_map)
-}
-
-let dataset = d3.json(url)
-let geoLayer
-
 // Retrieve and add the sighting data to the map
 dataset.then(function (data) {
-//d3.json(url).then(function (data) {
     geoLayer = L.geoJson(data, {
-
         pointToLayer: setMarkers,
-
         // Feature data popup
         onEachFeature: each_feature,
-
+        // Shape and Date filters
         filter: filterMap
-
     }).addTo(uap_map);
 
-    // Add the color legend
-    // add_legend(uap_map) 
+    // Update sighting info
+    sightingInfo()
  });
 
-
+ // Filter to shape
  function setShape(shape){
     shape_filter = shape
     unFilterMap()
  }
 
+ // Filter to date
  function setDate(){
     unFilterMap()
  }
 
+ // Display Sighting Info Card
+ function sightingInfo(){
+    let sightInfo = d3.select("#SightingHeader");
+    sightInfo.html('Sighting Info: ' + shape_filter + '<br />')
+    sightInfo = d3.select("#sample-metadata");
+    sightInfo.html('<b>Number of sightings:</b> ' + sightingsCount + '<br />' +
+        '<b>Last Date:</b> ' + sightingOccurred + '<br />' + 
+        '<b>Last Location:</b> ' + sightingCity + ', ' + sightingState + '<br />' + 
+        '<b>Last Summary:</b> ' + sightingSummary + '<br />'
+    )
+ }
+
+ // Run user filter
  function filterMap (feature, layer){
     fDate = new Date(feature.properties.Occurred)
     fYear = fDate.getFullYear()
@@ -213,27 +167,33 @@ dataset.then(function (data) {
     }
  }
 
- let markers 
- function unFilterMap (feature, layer){
 
+ // Reload map with filters
+ function unFilterMap (feature, layer){
     geoLayer.remove()
     markersOnMap = 0
+    sightingsCount = 0
+    sightingOccurred = ''
+    sightingCity = ''
+    sightingState = ''
+    sightingSummary = ''
     dataset.then(function (data) {
 
        geoLayer = L.geoJson(data, {
-
             pointToLayer: setMarkers,
-
             // Feature data popup
             onEachFeature: each_feature,
-
-
+            // Shape and Date filters
             filter: filterMap
         }).addTo(uap_map) 
+
+        // Update sighting info
+        sightingInfo()
     });
     
  }
 
+ // Populate the shape filter
  let dropdownMenu = d3.select("#selDataset");
  dropdownMenu.append("option").text("All").property("value");
  for (x in shapes){
